f693843fd963959979d574fd3c89675296d6e5b1
Mary Ann Jin <maryann.jin@gmail.com>
2015-09-30 01:33:43 -0400

    make it only activate when text is highlighted; get rid of empty popup

--- popup.js
+++ popup.js
@@ -1,3 +1,18 @@
-window.addEventListener('DOMContentLoaded', function() {
+function openReadWindow (text) {
+  if (text) {
     chrome.windows.create({'url': 'window.html', 'focused': false});
+  }
+  window.close();
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true
+    }, function(tabs) {
+        chrome.tabs.sendMessage(
+                tabs[0].id,
+                {from: 'popup', subject: 'text'},
+                openReadWindow);
+    });
 });
