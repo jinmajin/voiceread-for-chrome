75133f9f0ef7b44a9784abc7b954446ba380b19a
Mary Ann Jin <maryann.jin@gmail.com>
2015-09-30 01:24:03 -0400

    Make the reader open in new window rather than extension popup

--- popup.js
+++ popup.js
@@ -1,25 +1,3 @@
-function openHighlightedText(text) {
-    var words = text.split(/\s+/);
-    var line = document.createElement('div');
-    for(var i = 0; i < words.length; i++) {
-      if (i % 3 == 0 && i != 0) {
-        line.className = 'line';
-        document.getElementById('text').appendChild(line);
-        line = document.createElement('div');
-      }
-      line.innerHTML += words[i] + ' ';
-    }
-    document.getElementById('text').appendChild(line);
-}
-
 window.addEventListener('DOMContentLoaded', function() {
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function(tabs) {
-        chrome.tabs.sendMessage(
-                tabs[0].id,
-                {from: 'popup', subject: 'text'},
-                openHighlightedText);
-    });
+    chrome.windows.create({'url': 'window.html', 'focused': false});
 });
